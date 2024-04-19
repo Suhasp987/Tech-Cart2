73852b4f00295a7fe792d4db6c4f81ffeac492fd
Suhasp987 <ps9781094@gmail.com>
2024-04-19 07:27:33 +0530

    df

---
+++ testing/src/components/utils/constant.js
@@ -0,0 +1,13 @@
+// import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import HomeIcon from '@mui/icons-material/Home';
+
+
+
+
+export const Categories=[
+         {name:"Home",  icon:"Home",},
+         {name:"Cart" ,icon:"Hello",},
+         {name:'Account',icon:"Account",}
+         
+]
